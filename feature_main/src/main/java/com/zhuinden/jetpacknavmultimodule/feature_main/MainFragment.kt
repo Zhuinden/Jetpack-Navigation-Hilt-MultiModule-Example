1041400df25e01e842f84aee6030776403212f22
Gabor Varadi <zhuinden@gmail.com>
2021-05-26 22:39:01 +0200

    Actually get the args in MainFragment

--- feature_main/src/main/java/com/zhuinden/jetpacknavmultimodule/feature_main/MainFragment.kt
+++ feature_main/src/main/java/com/zhuinden/jetpacknavmultimodule/feature_main/MainFragment.kt
@@ -3,11 +3,14 @@ package com.zhuinden.jetpacknavmultimodule.feature_main
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import com.zhuinden.jetpacknavmultimodule.feature_main.databinding.MainFragmentBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainFragment: Fragment(R.layout.main_fragment) {
+    private val args by navArgs<MainFragmentArgs>()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
